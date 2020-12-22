b52df316bd76061f9fa83790efe3dad8981d7dc6
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-12-22 17:42:43 +0530

    change the productlist class

--- lib/product_list.rb
+++ lib/product_list.rb
@@ -1,10 +1,10 @@
 require_relative "products"
 
 class ProductList
-  def product_info
-    Products.new("milk", 3.97, 2, 2.5)
-    Products.new("bread", 2.17, 3, 2.00)
-    Products.new("banana", 0.99, 0, 0)
-    Products.new("apple", 0.89, 0, 0)
-  end
+  Product = {
+    "milk" => Products.new("milk", 3.97, 2, 2.5),
+    "bread" => Products.new("bread", 2.17, 3, 2.00),
+    "banana" => Products.new("banana", 0.99, 0, 0),
+    "apple" => Products.new("apple", 0.89, 0, 0),
+  }
 end
